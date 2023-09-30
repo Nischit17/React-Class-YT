86c78e1493d006ca04faca924004a017eb136662
unknown <nischit819@gmail.com>
2023-09-30 21:58:27 +0530

    episode-01-part-01(REDUX, API)

---
+++ src/components/Head.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-8 cursor-pointer"
+          alt="menu"
+          src="https://static.vecteezy.com/system/resources/previews/002/292/406/non_2x/hamburger-menu-line-icon-free-vector.jpg"
+        />
+        <a href="/">
+          <img
+            className="h-8 w-90 mx-6"
+            alt="yt-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png"
+          />
+        </a>
+      </div>
+      <div className="col-span-10 px-10 mx-60">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-200">
+          Search
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          alt="user-icon"
+          src="https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
