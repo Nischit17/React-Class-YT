ec3c364656f04bf10f078fa2ee98d89235312226
unknown <nischit819@gmail.com>
2023-10-07 16:52:00 +0530

    episode-01-part-01|-02-(useMemo Done)

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+import { findPrime } from "../utils/helper";
 
 const Demo = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(0);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  console.log("Rendering...");
+  // Heavy operation
+
+  const prime = useMemo(() => findPrime(text), [text]);
+
   return (
-    <div className="m-4 p-2 w-96 h-96 border border-black">
+    <div
+      className={
+        "m-4 p-2 w-96 h-96 border border-black " +
+        (isDarkTheme && "bg-gray-900 text-white")
+      }
+    >
+      <div>
+        <button
+          className="m-10 p-2 bg-green-400"
+          onClick={() => setIsDarkTheme(!isDarkTheme)}
+        >
+          Toggle
+        </button>
+      </div>
       <div>
         <input
           className="border border-black w-72 px-2"
-          type="text"
+          type="number"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
+      </div>
+      <div>
+        <h1 className="mt-4 font-bold text-xl">nth Prime : {prime}</h1>
       </div>
     </div>
   );
